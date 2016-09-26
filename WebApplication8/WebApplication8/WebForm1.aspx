8063c9234d307d55eb4826018aaa8fcefa1401f0
kriparson96 <kriparson96@gmail.com>
2016-09-26 10:32:10 -0700

    commit

---
+++ WebApplication8/WebApplication8/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication8.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+         <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <div>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <asp:Label ID="Label1" runat="server" Text="TimeLeft:"></asp:Label>
+                <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+            </ContentTemplate>
+            <Triggers>
+                <asp:AsyncPostBackTrigger ControlID="Timer1"  />
+            </Triggers>
+        </asp:UpdatePanel>
+        <asp:Timer ID="Timer1" runat="server" Interval="1000" OnTick="Timer1_Tick"></asp:Timer>
+    </div>
+
+          <asp:Label ID="Label3" runat="server"></asp:Label>
+
+          </form>
+</body>
+</html>
---
+++ WebApplication8/WebApplication8/WebForm1.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication8
+
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static int hh, mm, ss;
+
+        static double TimeAllSecondes = 10;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (TimeAllSecondes > 0)
+            {
+                TimeAllSecondes = TimeAllSecondes - 1;
+                     TimeSpan time_Span = TimeSpan.FromSeconds(TimeAllSecondes);
+                hh = time_Span.Hours;
+                mm = time_Span.Minutes;
+                ss = time_Span.Seconds;
+
+                Label2.Text = "  " + hh + ":" + mm + ":" + ss;
+
+            }
+            else
+            {
+                Label3.Text = "Time's up!!";
+            }
+           
+           
+        }
+           
+    }
+}
